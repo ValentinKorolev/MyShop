ca14f3b37bd19641a78fb09409e9303b34c7e8d5
KSONIX2 <52291949+KSONIX2@users.noreply.github.com>
2022-12-05 19:55:54 +0300

    Add base models for Catalog

---
+++ Models/CatalogBrand.cs
@@ -0,0 +1,9 @@
+﻿namespace MyShop.Models
+{
+    public sealed class CatalogBrand
+    {
+        //TODO replace to GUID
+        public int Id { get; set; }
+        public string Brand { get; set; }
+    }
+}
