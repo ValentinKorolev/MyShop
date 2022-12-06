72931adc7cd9fbce3df9d6501827d65f2c708037
KSONIX2 <52291949+KSONIX2@users.noreply.github.com>
2022-12-06 13:13:43 +0300

    Add Catalog controller(set as a defualt) + some action methods

--- Models/CatalogItem.cs
+++ Models/CatalogItem.cs
@@ -7,17 +7,15 @@
         public string Description { get; set; }
         public decimal Price { get; set; }
         public string PictureUrl { get; set; }
-        public CatalogType CatalogType { get; set; }
+        public CatalogType? CatalogType { get; set; }
         public CatalogBrand CatalogBrand { get; set; }
 
-        public CatalogItem(int id, string name, string description, decimal price, string pictureUrl)
+        public CatalogItem(string name, string description, decimal price, string pictureUrl)
         {
-            Id = id;
             Name = name;
             Description = description;
             Price = price;
-            PictureUrl = pictureUrl;
-            
+            PictureUrl = pictureUrl;           
         }
     }
 }
