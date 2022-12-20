2c1fa7bd02d99d638939c11dd10c6b1672fd5145
KSONIX2 <52291949+KSONIX2@users.noreply.github.com>
2022-12-20 22:03:48 +0300

    Restructure VS solution

---
+++ src/MyShop.Infastructure/LoggerAdapter.cs
@@ -0,0 +1,35 @@
+﻿using Microsoft.Extensions.Logging;
+using MyShop.ApplicationCore.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MyShop.Infastructure
+{
+    public sealed class LoggerAdapter<T> : IAppLogger<T>
+    {
+        private readonly ILogger<T> _logger;
+
+        public LoggerAdapter(ILoggerFactory logger)
+        {
+            _logger = logger.CreateLogger<T>();
+        }
+
+        public void LogError(Exception exception, string? message, params object[] arg)
+        {
+            _logger.LogError(exception, message, arg);
+        }
+
+        public void LogInformation(string message, params object[] arg)
+        {
+            _logger.LogInformation(message, arg);
+        }
+
+        public void LogWarning(string message, params object[] arg)
+        {
+            _logger.LogWarning(message, arg);
+        }
+    }
+}
